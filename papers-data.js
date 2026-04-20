10bd702d9e78bab55bc731f54d473c5d47da73d8
Sinclairwww <90683011+Sinclairwww@users.noreply.github.com>
2026-04-20 18:41:40 +0800

    update url

--- papers-data.js
+++ papers-data.js
@@ -11,7 +11,7 @@ const papers = [
     },
     {
         title: "UAV-Track VLA: Embodied Aerial Tracking via Vision-Language-Action Models",
-        url: "https://flying-intelligence.github.io/UAV-Track-VLA-Web/",
+        url: "https://flying-intelligence-team.github.io/UAV-Track-VLA-Web/",
         venue: "arXiv",
         img: "files/images/UAV-Track-VLA.png",
         date: "Apr. 2026",
